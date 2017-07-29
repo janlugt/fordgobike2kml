376216c259c0e5746db13347bf87c2b2c54590ac
Jan van der Lugt <janlugt@gmail.com>
2017-07-29 20:19:20 +0200

    .

--- fordgobike2kml.py
+++ fordgobike2kml.py
@@ -7,11 +7,21 @@ station_info = json.loads(response.read())['data']['stations']
 response = urllib2.urlopen('https://gbfs.fordgobike.com/gbfs/en/station_status.json')
 station_status = json.loads(response.read())['data']['stations']
 
+installed_style = simplekml.Style()
+installed_style.iconstyle.icon.href = 'https://member.fordgobike.com/versions/1500489851/8dResources/eightdbike/images/maps/markers/in-service/marker_100.png'
+planned_style = simplekml.Style()
+planned_style.iconstyle.icon.href = 'https://member.fordgobike.com/versions/1500489851/8dResources/eightdbike/images/maps/planned.png'
+
 kml = simplekml.Kml(name = 'Ford GoBike locations')
 
 for info in station_info:
   status = [x for x in station_status if x['station_id'] == info['station_id']][0]
   pnt = kml.newpoint(name='<![CDATA[%s]]>' % info['name'])
   pnt.coords = [(info['lon'], info['lat'])]
+
+  if status['is_installed']:
+    pnt.style = installed_style
+  else:
+    pnt.style = planned_style
 
 kml.save('fordgobike.kml')
